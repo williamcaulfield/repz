df1e665dc3cf3e81629c859bdd4472befd2b1f37
williamcaulfield <williamcaulfield@hotmail.com>
2021-10-26 07:47:55 +0200

    fixing workout builder

--- app/views/Workouts/workoutCreate/workoutCreate.vue
+++ app/views/Workouts/workoutCreate/workoutCreate.vue
@@ -1,6 +1,11 @@
 <template>
   <StackLayout class="main">
-    <ContentView class="wo-add__container" id="myView" @tap="onClickButton()">
+    <ContentView
+      class="wc-add__container_primary"
+      id="createWorkout"
+      @tap="onClickButton()"
+      @touch="onTouch($event)"
+    >
       <GridLayout rows="auto,*" columns="auto,*,auto">
         <Image
           col="1"
@@ -25,6 +30,17 @@
 <script>
 export default {
   methods: {
+    onTouch(args) {
+      const contentView = args.object;
+      switch (args.action) {
+        case "up":
+          contentView.deletePseudoClass("pressed");
+          break;
+        case "down":
+          contentView.addPseudoClass("pressed");
+          break;
+      }
+    },
     onClickButton() {
       this.$emit("clicked");
     },
@@ -37,13 +53,46 @@ export default {
 // End custom common variables
 
 // Custom styles
+
+.wc-add__container_primary {
+  width: 90%;
+  border-radius: 10;
+  // border-color: white;
+  // border-width: $border-width;
+  margin-top: 15;
+  margin-bottom: 15;
+  height: 80;
+  background-color: $background-gradient-dark;
+
+  background-image: linear-gradient(
+    to bottom,
+    $gradient-active-light,
+    rgb(111, 1, 1)
+  );
+
+  //   background-image: linear-gradient(
+  //   to bottom,
+  //   $container-gradient-light,
+  //   $container-gradient-dark
+  // ;
+}
+.wc-add__container_primary:pressed {
+  background-color: $background-gradient-light;
+
+  background-image: linear-gradient(
+    to bottom,
+    $gradient-active-dark,
+    $container-gradient-dark
+  );
+}
+
 .wo-add__container {
   width: 90%;
   margin: auto;
   border-radius: 10;
 
   border-width: $border-width;
-  margin-top: 10;
+  margin-top: 15;
   margin-bottom: 10;
   height: 80;
   background-image: linear-gradient(
